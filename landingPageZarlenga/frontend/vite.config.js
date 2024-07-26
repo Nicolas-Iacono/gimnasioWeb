88cb5905f203e803a5973757461e8da5a595045c
Nicolas Iacono <nicoiacono96@gmail.com>
2024-07-26 16:25:38 -0300

    cambiando endpoint de localhost:3000 a re

--- landingPageZarlenga/frontend/vite.config.js
+++ landingPageZarlenga/frontend/vite.config.js
@@ -6,12 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://movimiento-consiente.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-  },
+   
+
 },
 })
