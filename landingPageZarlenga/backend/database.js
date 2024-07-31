d30ef0c8ebbeec551e6d74a6667a9672501bdabd
Nicolas Iacono <nicoiacono96@gmail.com>
2024-07-31 14:02:03 -0300

    [BACK]creando base de datos con sequelize

--- landingPageZarlenga/backend/database.js
+++ landingPageZarlenga/backend/database.js
@@ -1,20 +1,86 @@
 const mysql = require('mysql2/promise');
 const dotenv = require("dotenv");
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 dotenv.config();
+
+const sequelize = new Sequelize('mov-consiente', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+const User = sequelize.define('User', {
+  idUser: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  height: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  weight: {
+    type: DataTypes.DECIMAL(10,2),
+    allowNull: false,
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  },{
+    timestamps: false, // Desactiva createdAt y updatedAt
+  });
+
+const Message = sequelize.define('Message', {
+  idMessage: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  userId : {
+    type: DataTypes.INTEGER,
+    references:{
+      model: User,
+      key: 'idUser',
+    },
+  },
+},{
+  timestamps:false,
+});
+
+User.hasOne(Message,{foreignKey:'userId'});
+Message.belongsTo(User,{foreignKey:'userId'})
+sequelize.sync();
 
 const connection = mysql.createPool({
   host: "localhost",
-  database: "app_mc",
+  database: "mov-consiente",
   user: "root",
-  password: "root"
+  password: "root",
 });
 
 async function testConnection() {
   try {
-    const conn = await connection.getConnection(); // Usa la instancia del pool de conexiones
+    const conn = await connection.getConnection();
     console.log('Conexión a la base de datos ha sido establecida exitosamente.');
-    conn.release(); // Libera la conexión al pool
+    conn.release();
   } catch (err) {
     console.error('No se pudo conectar a la base de datos:', err);
   }
@@ -22,7 +88,7 @@ async function testConnection() {
 
 async function testQuery() {
   try {
-    const [rows, fields] = await connection.query('SELECT 1 + 1 AS solution'); // Usa la instancia del pool de conexiones
+    const [rows, fields] = await connection.query('SELECT 1 + 1 AS solution');
     console.log('La solución es:', rows[0].solution);
   } catch (err) {
     console.error('Error en la consulta:', err);
